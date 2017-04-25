8636219c2115f01e2ec49045a796493f092ea7ae
Andreas <rednex702@hotmail.com>
2017-04-25 08:17:07 +0200

    actor and movie classes

---
+++ Actor.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class Actor
+{
+     private String firstname;
+     private String lastname;
+     private ArrayList<movies> movies;
+     private Date birthday;
+
+     public Actor(firstname String, lastname String, movies ArrayList<movies>, birthday Date)
+     {
+         this.firstname = firstname;
+         this.lastname = lastname;
+         this.username = username;
+     }
+     
+     
+
+
+
+}
---
+++ Movie.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Movie
+{
+    private String title;
+    private int releaseYear;
+    private ArrayList<Receipt> receiptList = new ArrayList<Receipt>();
+
+
+}
